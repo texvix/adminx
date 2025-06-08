344f07d0e220c5e1c6c8d87fd7f742e9704b8adb
texvix <texvixx@gmail.com>
2025-06-08 13:39:52 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">Willkommen bei Texvix Admin</h1>
+      <h1 className="text-2xl font-bold">Willkommen bei Texvix sdda</h1>
     </Layout>
   );
 }
